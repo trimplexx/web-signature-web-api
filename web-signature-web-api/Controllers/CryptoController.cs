58df2786426ae53c11ac4899ffb613b8306ad586
filip <filip.habryn@gmail.com>
2023-11-13 21:22:54 +0100

    Fix Implementation of key management for logged-in users so that the private key is not transmitted

--- web-signature-web-api/Controllers/CryptoController.cs
+++ web-signature-web-api/Controllers/CryptoController.cs
@@ -19,41 +19,65 @@ namespace web_signature_web_api.Controllers
             _context = context;
         }
 
-        [HttpPost("generate-keys")]
-        public async Task<IActionResult> GenerateKeys(UserIdRequest userIdRequest)
+        /*    [HttpPost("generate-keys")]
+            public async Task<IActionResult> GenerateKeys(UserIdRequest userIdRequest)
+            {
+                // Wyszukanie użytkownika o podanym Id
+                var user = await _context.Users.FindAsync(userIdRequest.Id);
+                if (user == null)
+                {
+                    return NotFound(new { error = "UserNotFound", message = "Nie znaleziono użytkownika" });
+                }
+
+                //Generowanie Kluczy
+                using var rsa = new RSACryptoServiceProvider(2048);
+                try
+                {
+                    var publicKey = rsa.ToXmlString(false);
+                    var privateKey = rsa.ToXmlString(true);
+
+                    var userPublicKey = new UserPublicKey
+                    {
+                        UserId = userIdRequest.Id,
+                        PublicKey = publicKey,
+                        CreatedAt = DateTime.UtcNow,
+                        ExpiresAt = DateTime.UtcNow.AddYears(1)
+                    };
+                    _context.UserPublicKeys.Add(userPublicKey);
+                    await _context.SaveChangesAsync();
+
+                    return Ok(new { privateKey = privateKey });
+                }
+                catch (Exception ex)
+                {
+                    return BadRequest(new { message = "Wystąpił błąd podczas generowania kluczy.", exception = ex.Message });
+                }
+            }*/
+
+        [HttpPost("upload-public-key")]
+        public async Task<IActionResult> UploadPublicKey([FromBody] PublicKeyRequest request)
         {
             // Wyszukanie użytkownika o podanym Id
-            var user = await _context.Users.FindAsync(userIdRequest.Id);
+            var user = await _context.Users.FindAsync(request.UserId);
             if (user == null)
             {
                 return NotFound(new { error = "UserNotFound", message = "Nie znaleziono użytkownika" });
             }
 
-            //Generowanie Kluczy
-            using var rsa = new RSACryptoServiceProvider(2048);
-            try
+            var userPublicKey = new UserPublicKey
             {
-                var publicKey = rsa.ToXmlString(false);
-                var privateKey = rsa.ToXmlString(true);
-
-                var userPublicKey = new UserPublicKey
-                {
-                    UserId = userIdRequest.Id,
-                    PublicKey = publicKey,
-                    CreatedAt = DateTime.UtcNow,
-                    ExpiresAt = DateTime.UtcNow.AddYears(1)
-                };
-                _context.UserPublicKeys.Add(userPublicKey);
-                await _context.SaveChangesAsync();
-
-                return Ok(new { privateKey = privateKey });
-            }
-            catch (Exception ex)
-            {
-                return BadRequest(new { message = "Wystąpił błąd podczas generowania kluczy.", exception = ex.Message });
-            }
+                UserId = request.UserId,
+                PublicKey = request.PublicKey,
+                CreatedAt = DateTime.UtcNow,
+                ExpiresAt = DateTime.UtcNow.AddYears(1)
+            };
+            _context.UserPublicKeys.Add(userPublicKey);
+            await _context.SaveChangesAsync();
+            
+            return Ok(new { message = "Klucz publiczny przesłany pomyślnie." });
         }
 
+        // Metoda do sprawdzenia, czy klucz istnieje
         [HttpGet("check-key-exists")]
         public async Task<IActionResult> CheckKeyExists([FromQuery] int userId)
         {
---
+++ web-signature-web-api/Models/PublicKeyRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace web_signature_web_api.Models
+{
+    public class PublicKeyRequest
+    {
+        public int UserId { get; set; }
+        public string PublicKey { get; set; }
+    }
+}
